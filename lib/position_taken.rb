a4d9cec83e4b519606a2eca7e8ac6facfb7ad0db
Marie Burns <mcburns2222@gmail.com>
2018-10-27 18:06:48 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@
 def position_taken?(board, index)
   if board[index] == " " || "" || nil
     return FALSE
-  elsif board[index] == "X" || "Y"
+  elsif board[index] == "X" || "O"
     return TRUE
   end
 end
